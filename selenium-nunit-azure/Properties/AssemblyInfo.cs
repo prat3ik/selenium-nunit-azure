db0422683ee0c27cde79c3d2fdc4883332258332
prat3ik <pratikquora@gmail.com>
2019-01-22 17:23:30 +0530

    Sample Test Case added

---
+++ selenium-nunit-azure/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("selenium-nunit-azure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("selenium-nunit-azure")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b5ecf56-27da-4d0b-9b56-4db6562566f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ selenium-nunit-azure/TestClass.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_nunit_azure
+{
+
+    public class TestClass
+    {
+
+        ChromeDriver driver;
+
+        [SetUp]
+        public void SetUpMethod()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void VerifyUserCanSearchOnGoogle()
+        {
+            int searchIndex = 0;
+
+            // Navigate to Google.
+            driver.Url = "https://www.google.com";
+
+            // Search for the "Test" keyword.
+            driver.FindElement(By.CssSelector("[title=\"Search\"]")).SendKeys("Test" + Keys.Enter);
+
+            // Get the text of first searched result.
+            string expectedHeaderTextOfWebsite = driver.FindElements(By.CssSelector(".rc>.r>a>h3"))[searchIndex].Text;
+
+            // Select the first link from searched result.
+            driver.FindElements(By.CssSelector(".rc>.r>a"))[searchIndex].Click();
+
+            // Get the actual text of header for navigated website.
+            string actualHeaderTextOfWebsite = driver.Title;
+
+            // Assert that expected website header(from Google search result) and actual website header text(from navigated website) matches.
+            Assert.AreEqual(expectedHeaderTextOfWebsite, actualHeaderTextOfWebsite, "Header text of (first)navigated website didn't match!");
+
+        }
+
+        [TearDown]
+        public void TearDownMethod()
+        {
+            driver.Quit();
+        }
+
+    }
+}
